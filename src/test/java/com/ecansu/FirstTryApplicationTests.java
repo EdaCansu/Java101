99d533b0b72d64e3c8069c76e8e47408517b6704
EdaCansu <edamiaj@gmail.com>
2022-10-19 23:37:23 +0300

    Package, Class, Method, Main, Sout ve kacis elemanlari

---
+++ src/test/java/com/ecansu/FirstTryApplicationTests.java
@@ -0,0 +1,18 @@
+package com.ecansu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTryApplicationTests {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+}
