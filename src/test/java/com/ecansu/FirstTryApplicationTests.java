a6864fb78ff81d490eebd0402fd6d6d5e5594492
EdaCansu <edamiaj@gmail.com>
2022-10-20 15:23:45 +0300

    variables and integer examples

--- src/test/java/com/ecansu/FirstTryApplicationTests.java
+++ src/test/java/com/ecansu/FirstTryApplicationTests.java
@@ -21,7 +21,8 @@ class FirstTryApplicationTests {
         int alan = kisaKenar * uzunKenar;
         int cevre = 2 * (kisaKenar + uzunKenar);
         System.out.println(alan);
-        System.out.println(cevre); */
+        System.out.println(cevre);
+        */
 
     }
 
