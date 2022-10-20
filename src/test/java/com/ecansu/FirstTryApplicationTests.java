8a634834bcbb631fa4fea4daa613e2345acce98d
EdaCansu <edamiaj@gmail.com>
2022-10-20 15:22:18 +0300

    variables and integer examples

--- src/test/java/com/ecansu/FirstTryApplicationTests.java
+++ src/test/java/com/ecansu/FirstTryApplicationTests.java
@@ -7,7 +7,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 class FirstTryApplicationTests {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+     //   System.out.println("Hello World");
+      /*        int number1 = 10;
+        System.out.println("number1");
+        int number1, number2 = 20, number3;
+        number1 = 10;
+        System.out.println(number1 + number2);
+
+        number3 = number1 + number2;
+        System.out.println(number3);
+
+        int kisaKenar = 10, uzunKenar = 20;
+        int alan = kisaKenar * uzunKenar;
+        int cevre = 2 * (kisaKenar + uzunKenar);
+        System.out.println(alan);
+        System.out.println(cevre); */
 
     }
 
