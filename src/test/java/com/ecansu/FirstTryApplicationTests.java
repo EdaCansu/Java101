154d2477b2f6b1e76bcbac78b4c99c0383ebda43
EdaCansu <edamiaj@gmail.com>
2022-10-20 17:26:02 +0300

    char and boolean

--- src/test/java/com/ecansu/FirstTryApplicationTests.java
+++ src/test/java/com/ecansu/FirstTryApplicationTests.java
@@ -24,7 +24,7 @@ class FirstTryApplicationTests {
         System.out.println(cevre);
         */
 
-        byte vByte = 1;
+        /* byte vByte = 1;
         short vShrt = 2;
         int vInt = 3;
         long vLong = 4;
@@ -36,7 +36,24 @@ class FirstTryApplicationTests {
         float vFloat = 3.14f;
         double vDouble = 4.14;
         System.out.println(vFloat);
-        System.out.println(vDouble);
+        System.out.println(vDouble); */
+
+        char vChar = 'b';
+        char v2Char = 98;
+        System.out.println(vChar);
+        System.out.println(v2Char);
+
+        char vNew = 'J';
+        char v2New = 'A';
+        char v3New = 'V';
+        System.out.println("" + vNew + v2New + v3New + v2New);
+        System.out.println(vNew + v2New + v3New + v2New);
+
+        boolean dogru = true;
+        boolean yanlis =  false;
+        System.out.println(dogru);
+        System.out.println(yanlis);
+
 
     }
 
