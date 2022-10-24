ab629f276c0d88bda2ee6c855a7aad617599ae54
EdaCansu <edamiaj@gmail.com>
2022-10-24 20:45:08 +0300

    karsilastirma operatorleri

---
+++ src/test/java/com/ecansu/SecondWorkingArea.java
@@ -0,0 +1,15 @@
+package com.ecansu;
+
+public class SecondWorkingArea {
+    public static void main(String[] args) {
+        // Karsilastirma operatorleri baslangic
+        /* == Eşittir ( x == y )
+        != Eşit Değildir ( x != y )
+        > Büyüktür ( x > y )
+        < Küçüktür ( x < y )
+        >= Büyük Eşittir ( x >= y )
+        <= Küçük Eşittir ( x <= y ) */
+        // Karsilastirma operatorleri son
+
+    }
+}
