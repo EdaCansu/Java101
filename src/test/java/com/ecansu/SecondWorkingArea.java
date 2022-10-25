e5abaff25e09b19a7a310931592085b642b94070
EdaCansu <edamiaj@gmail.com>
2022-10-25 20:08:56 +0300

    if-else bloklari

--- src/test/java/com/ecansu/SecondWorkingArea.java
+++ src/test/java/com/ecansu/SecondWorkingArea.java
@@ -20,5 +20,16 @@ public class SecondWorkingArea {
         */
         // Mantiksal operatorler son
 
+        // if-else bloklari baslangic
+        int a = 10, b = 20, c = 1;
+        if ((a < b) && (a < c)) {
+            System.out.println("A en küçük sayıdır.");
+        } else if ((b < a) && (b < c)) {
+            System.out.println("B en küçük sayıdır.");
+        } else {
+            System.out.println("C en küçük sayıdır.");
+        }
+        System.out.println("Programınız sonlandı!");
+        // if-else bloklari son
     }
 }
