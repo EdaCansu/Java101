f982380e0dc89752369003add0f569a72f4be27c
edacansu <edamiaj@gmail.com>
2022-10-28 14:50:19 +0300

    switch case ile burç bulma

--- src/test/java/com/ecansu/SecondWorkingArea.java
+++ src/test/java/com/ecansu/SecondWorkingArea.java
@@ -183,7 +183,7 @@ public class SecondWorkingArea {
         //Hava sıcaklığına göre etkinlik önerme son
 
         //Sayı sıralama başlangıç
-        int n1, n2, n3;
+        /* int n1, n2, n3;
         Scanner inp = new Scanner(System.in);
         System.out.print("Lütfen birinci sayıyı giriniz: ");
         n1 = inp.nextInt();
@@ -197,29 +197,136 @@ public class SecondWorkingArea {
             if (n2 > n3){
                 System.out.println(n2);
                 System.out.println(n3);
+                System.out.println(n1 + " > " + n2 + " > " + n3);
             } else {
                 System.out.println(n3);
                 System.out.println(n2);
+                System.out.println(n1 + " > " + n3 + " > " + n2);
             }
         } else if (n2 > n1 && n2 > n3) {
             System.out.println(n2);
             if (n1 > n3){
                 System.out.println(n1);
                 System.out.println(n3);
+                System.out.println(n2 + " > " + n1 + " > " + n3);
             } else {
                 System.out.println(n3);
                 System.out.println(n1);
+                System.out.println(n2 + " > " + n3 + " > " + n1);
             }
         } else {
             System.out.println(n3);
             if (n1 > n2){
                 System.out.println(n1);
                 System.out.println(n2);
+                System.out.println(n3 + " > " + n1 + " > " + n2);
             } else {
                 System.out.println(n2);
                 System.out.println(n1);
+                System.out.println(n3 + " > " + n2 + " > " + n1);
             }
-        }
+        } */
         //Sayı sıralama son
+
+        //Burç bulma başlangıç
+        int bDay, month;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Lütfen doğduğunuz ayı giriniz: ");
+        month = inp.nextInt();
+        System.out.print("Lütfen doğduğunuz günü giriniz: ");
+        bDay = inp.nextInt();
+
+        switch (month){
+            case 1:
+                if (bDay < 22) {
+                    System.out.println("Burcunuz: Oğlak!");
+                } else {
+                    System.out.println("Burcunuz: Kova!");
+                }
+                break;
+            case 2:
+                if (bDay < 20) {
+                    System.out.println("Burcunuz: Kova!");
+                } else {
+                    System.out.println("Burcunuz: Balık!");
+                }
+                break;
+            case 3:
+                if (bDay < 21) {
+                    System.out.println("Burcunuz: Balık!");
+                } else {
+                    System.out.println("Burcunuz: Koç!");
+                }
+                break;
+            case 4:
+                if (bDay < 21) {
+                    System.out.println("Burcunuz: Koç!");
+                } else {
+                    System.out.println("Burcunuz: Boğa!");
+                }
+                break;
+            case 5:
+                if (bDay < 22) {
+                    System.out.println("Burcunuz: Boğa!");
+                } else {
+                    System.out.println("Burcunuz: İkizler!");
+                }
+                break;
+            case 6:
+                if (bDay < 23) {
+                    System.out.println("Burcunuz: İkizler!");
+                } else {
+                    System.out.println("Burcunuz: Yengeç!");
+                }
+                break;
+            case 7:
+                if (bDay < 23) {
+                    System.out.println("Burcunuz: Yengeç!");
+                } else {
+                    System.out.println("Burcunuz: Aslan!");
+                }
+                break;
+            case 8:
+                if (bDay < 23) {
+                    System.out.println("Burcunuz: Aslan!");
+                } else {
+                    System.out.println("Burcunuz: Başak!");
+                }
+                break;
+            case 9:
+                if (bDay < 23) {
+                    System.out.println("Burcunuz: Başak!");
+                } else {
+                    System.out.println("Burcunuz: Terazi!");
+                }
+                break;
+            case 10:
+                if (bDay < 23) {
+                    System.out.println("Burcunuz: Terazi!");
+                } else {
+                    System.out.println("Burcunuz: Akrep!");
+                }
+                break;
+            case 11:
+                if (bDay < 22) {
+                    System.out.println("Burcunuz: Akrep!");
+                } else {
+                    System.out.println("Burcunuz: Yay!");
+                }
+                break;
+            case 12:
+                if (bDay < 22) {
+                    System.out.println("Burcunuz: Yay!");
+                } else {
+                    System.out.println("Burcunuz: Oğlak!");
+                }
+                break;
+            default:
+                System.out.println("Hatalı bilgi girdiniz:");
+
+        }
+        //Burç bulma son
+
+
     }
 }
