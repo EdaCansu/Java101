1c7cf37ddc845b9f8cf5144a332309eb528d223e
EdaCansu <edamiaj@gmail.com>
2022-11-06 00:27:11 +0300

    Recursive and Fibonacci series start

--- src/test/java/com/ecansu/FourthWorkingArea.java
+++ src/test/java/com/ecansu/FourthWorkingArea.java
@@ -1,8 +1,8 @@
 package com.ecansu;
 
 public class FourthWorkingArea {
-
-    static boolean isPalindrom(int number) {
+    //Palindrom başlangıç
+   /* static boolean isPalindrom(int number) {
         int temp = number, reverseNumber = 0, lastNumber;
         while (temp != 0) {
             lastNumber = temp % 10;
@@ -13,9 +13,25 @@ public class FourthWorkingArea {
             return true;
         else
             return false;
+    }*/
+    // Palindrom son
+
+    //Recursive and Fibonacci series start
+
+    static int fib(int n) {
+        if (n == 1 || n== 2) {
+            return 1;
+        }
+        return fib(n-1)+ fib(n-2);
     }
+    //Recursive and Fibonacci series end
 
     public static void main(String[] args) {
-        System.out.println(isPalindrom(2442));
+        //- Palindrom -- System.out.println(isPalindrom(2442));
+
+        // Recursive and Fibonacci series
+        for(int i = 1; i<10; i++ ){
+        System.out.println(fib(i));
+        }
     }
 }
