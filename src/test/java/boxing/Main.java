9cec1adfe13070370df602f97b72432c4abcc493
EdaCansu <edamiaj@gmail.com>
2022-11-08 13:38:40 +0300

    boxing

---
+++ src/test/java/boxing/Main.java
@@ -0,0 +1,12 @@
+package boxing;
+
+public class Main {
+    public static void main(String[] args) {
+        Fighter f1 = new Fighter("Alex", 10, 80, 89, 60);
+        Fighter f2 = new Fighter("Souza", 15, 75, 91, 70);
+
+        Match match = new Match(f1, f2, 85, 100);
+        match.run();
+
+    }
+}
