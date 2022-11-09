3e1809cfaeaf30882e4b98aa378c3e741073a951
EdaCansu <edamiaj@gmail.com>
2022-11-09 17:39:22 +0300

    array giriş

---
+++ src/test/java/arrayss/Arrayler.java
@@ -0,0 +1,18 @@
+package arrayss;
+
+public class Arrayler {
+
+    public static void main(String[] args) {
+        int[] list = {10, 20, 30, 40, 50};
+
+        /*double avarage = 0;
+        for(int i : list){
+            avarage += i;
+        }
+        avarage /= list.length;
+        System.out.println(avarage);*/
+
+
+    }
+
+}
