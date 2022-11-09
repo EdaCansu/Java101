4e8f96c607f9ec80180d4e2a27deda6a1a01a55a
EdaCansu <edamiaj@gmail.com>
2022-11-09 18:00:01 +0300

    arraylerde min max değer bulma

--- src/test/java/arrayss/Arrayler.java
+++ src/test/java/arrayss/Arrayler.java
@@ -1,9 +1,30 @@
 package arrayss;
 
+import java.util.Arrays;
+
 public class Arrayler {
 
     public static void main(String[] args) {
-        int[] list = {10, 20, 30, 40, 50};
+        int[] list = {59, 57, 81, -240, 80};
+        int[] list2 = {5, 14, 7, 11, 9};
+        Arrays.sort(list2);
+
+        int number = 13, minNumber = list2[0], maxNumber = list2[0];
+        for (int i: list2) {
+            if (i < number) {
+               minNumber = i;
+            }
+
+            if(i > number){
+                maxNumber =i;
+                break;
+            }
+        }
+
+        System.out.println("en küçük number " + minNumber);
+        System.out.println("En büyük number " + maxNumber);
+
+
 
         /*double avarage = 0;
         for(int i : list){
@@ -11,6 +32,20 @@ public class Arrayler {
         }
         avarage /= list.length;
         System.out.println(avarage);*/
+
+        /*int min = list[0], max = list[0];
+        for(int i = 0; i < list.length; i++){
+            if (list[i] < list[0]) {
+                min = list[i];
+            }
+            if (list[i] > list[0]) {
+                max = list[i];
+            }
+        }
+
+        System.out.println("min   " + min);
+        System.out.println("max    " + max);*/
+
 
 
     }
