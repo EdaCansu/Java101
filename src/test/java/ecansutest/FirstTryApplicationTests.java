ffe1ffb42c71a61534dbf50997c89692ca1c8192
EdaCansu <edamiaj@gmail.com>
2022-12-02 14:40:26 +0300

    revise

--- src/test/java/ecansutest/FirstTryApplicationTests.java
+++ src/test/java/ecansutest/FirstTryApplicationTests.java
@@ -1,8 +1,7 @@
-package com;
+package ecansutest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import java.util.Scanner;
 
 @SpringBootTest
 class FirstTryApplicationTests {
